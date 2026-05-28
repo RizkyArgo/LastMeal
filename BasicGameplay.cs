a6390e0bf1fb935d83ce7895470aceb97506290c
RizkyArgo <littleozil28@gmail.com>
2026-05-28 16:33:32 +0700

    tambah ui

--- BasicGameplay.cs
+++ BasicGameplay.cs
@@ -6,10 +6,12 @@ public class BasicGameplay : MonoBehaviour
     private Vector2 movement;
     private Animator anim;
     public bool sampah = false;
-    public GameObject button;
+    public GameObject E;
+    public GameObject miniGame;
     public GameObject kamera;
     float batasXkiri = 5.0f;
     float batasXkanan = 4.5f;
+    bool mulai = false;
 
     void Start()
     {
@@ -19,15 +21,19 @@ public class BasicGameplay : MonoBehaviour
 
     void Update()
     {
+        if (mulai == true){
         float hInput = Input.GetAxisRaw("Horizontal");
         float vInput = Input.GetAxisRaw("Vertical");
         movement = new Vector2(hInput, vInput).normalized;
         anim.SetFloat("velocityX", hInput);
         anim.SetFloat("velocityY", vInput);
-        // if (transform.position.x < -30)
-        // {
-        //     kamera.transform.position = new Vector3(-1f, kamera.transform.position.y, kamera.transform.position.z);
-        // } 
+        }
+
+        if (sampah && Input.GetKeyDown(KeyCode.E))
+            {
+                miniGame.SetActive(true);
+                E.SetActive(false);
+            }
     }
 
     void FixedUpdate()
@@ -40,7 +46,7 @@ public class BasicGameplay : MonoBehaviour
         if (collision.CompareTag("Sampah"))
         {
             sampah = true;
-            button.SetActive(true);
+            E.SetActive(true);
         }
     }
 
@@ -49,7 +55,12 @@ public class BasicGameplay : MonoBehaviour
         if (collision.CompareTag("Sampah"))
         {
             sampah = false;
-            button.SetActive(false);
+            E.SetActive(false);
         }
+    }
+
+    public void StartGame()
+    {
+        mulai = true;
     }
 }
--- MiniGame.cs
+++ MiniGame.cs
@@ -1,6 +1,6 @@
-using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class MiniGame : MonoBehaviour
 {
@@ -8,10 +8,12 @@ public class MiniGame : MonoBehaviour
     public RandomReward rng;
     public GameObject tutup;
     private RectTransform hitArea;
+    private RectTransform spasiArea;
     private RectTransform ijoArea;
     public GameObject hit;
     public Slider proggres;
     public GameObject kanvas;
+    public GameObject spasi;
 
     public float kecepatan = 200f;
     private bool naik = true;
@@ -19,11 +21,12 @@ public class MiniGame : MonoBehaviour
 
     void Start()
     {
+        spasiArea = spasi.GetComponent<RectTransform>();
         rectTransform = GetComponent<RectTransform>();
         hit = GameObject.Find("hit");
         hitArea = hit.GetComponent<RectTransform>();
         proggres.value = 0;
-        proggres.maxValue = 4;
+        proggres.maxValue = Random.Range(3, 5);
     }
 
     void Update()
@@ -48,11 +51,14 @@ public class MiniGame : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            spasi.transform.localScale = new Vector3(0.73f, 0.354369f, 0.354369f);
+
             if (masuk == true)
             {
                 proggres.value += 1;
                 pindahTarget();
-                if (proggres.value >= 4)
+
+                if (proggres.value >= proggres.maxValue)
                 {
                     kanvas.SetActive(false);
                     rng.Hadiah();
@@ -60,10 +66,12 @@ public class MiniGame : MonoBehaviour
                     tutup.SetActive(true);
                 }
             }
-            else
-            {
-                
-            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Vector3 awal = new Vector3(1f,0.4854369f,0.4854369f);
+            spasi.transform.localScale = awal;
         }
     }
 
@@ -85,10 +93,7 @@ public class MiniGame : MonoBehaviour
 
     void pindahTarget()
     {
-        float posisiBaru = Random.Range(-44,44);
-        hit.GetComponent<RectTransform>().anchoredPosition = new Vector2(hit.GetComponent<RectTransform>().anchoredPosition.x,posisiBaru);
+        float posisiBaru = Random.Range(-44, 44);
+        hit.GetComponent<RectTransform>().anchoredPosition = new Vector2(hit.GetComponent<RectTransform>().anchoredPosition.x, posisiBaru);
     }
-
-   
-    
 }
